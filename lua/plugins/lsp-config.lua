53c7eb5ebd496f8dd4b391884d7a36dc40733d5d
bit-aloo <sshourya17@gmail.com>
2025-10-19 21:51:25 +0530

    format the files

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,35 +1,65 @@
 return {
-  {"mason-org/mason.nvim",
-    opts = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
-        }
-    },
-    config = function()
-    require("mason").setup()
-    end
-  },
-  {
-      "mason-org/mason-lspconfig.nvim",
-      opts = {},
-      config = function()
-      require("mason-lspconfig").setup({
-        ensure = { "lua_ls", "rust_analyzer" }
-      })
-      end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      vim.lsp.config("lua_ls", {})
-      vim.lsp.config("rust_analyzer", {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure = { "lua_ls", "rust_analyzer" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.config("lua_ls", {})
+			--	vim.lsp.config("rust_analyzer", {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^6", -- Recommended
+		lazy = false, -- This plugin is already lazy
+		config = function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			vim.keymap.set("n", "<leader>a", function()
+				vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+				-- or vim.lsp.buf.codeAction() if you don't want grouping.
+			end, { silent = true, buffer = bufnr })
+			vim.keymap.set(
+				"n",
+				"K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+				function()
+					vim.cmd.RustLsp({ "hover", "actions" })
+				end,
+				{ silent = true, buffer = bufnr }
+			)
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			vim.keymap.set("n", "<leader>dt", ":DapToggleBreakpoint<CR>")
+			vim.keymap.set("n", "<leader>dx", ":DapTerminate<CR>")
+			vim.keymap.set("n", "<leader>do", "DapStepOver<CR>")
+		end,
+	},
+	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
 }
--- lua/plugins/matrix.lua
+++ lua/plugins/matrix.lua
@@ -1,12 +1,11 @@
 return {
-    "iruzo/matrix-nvim",
-    priority = 1000,
-    config = function()
-      
-      vim.g.matrix_contrast = true
-      vim.g.matrix_borders = false
-      vim.g.matrix_disable_background = false
-      vim.g.matrix_italic = true
-      vim.cmd.colorscheme("matrix")
-    end,
+	"iruzo/matrix-nvim",
+	priority = 1000,
+	config = function()
+		vim.g.matrix_contrast = true
+		vim.g.matrix_borders = false
+		vim.g.matrix_disable_background = false
+		vim.g.matrix_italic = true
+		vim.cmd.colorscheme("matrix")
+	end,
 }
